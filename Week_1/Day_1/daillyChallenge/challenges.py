25897102e6b2a465c6eea3d9ac98346dae0c9064
Martial Dessouza N'gado <guymartialngado@gmail.com>
2026-05-19 22:42:32 +0000

    correcting mistakes

--- Week_1/Day_1/daillyChallenge/challenges.py
+++ Week_1/Day_1/daillyChallenge/challenges.py
@@ -1,21 +1,21 @@
 #Challenge 1
 number = int(input("Enter a number: "))
-length = int(inputr("Enter the length of the list: "))
+length = int(input("Enter the length of the list: "))
 
 multiples = []
 for i in range(1, length +1):
     multiples.append(number * i)
-    print(multiples)
+print(multiples)
 
 
 
 
-#Challenge 2 removing consecutive identique characteres
+#Challenge 2 removing consecutive identical characteres
 text = input("Enter a string: ")
 result = text[0]
 for char in text[1:]:
     if char != result[-1]:
         result += char
-    print("New String:", result)
+print("New String:", result)
 
 
